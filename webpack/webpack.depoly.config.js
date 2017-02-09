378d0fde9f21600a0925ac634474038cf02e78ed
锡炎 <xiyan.xxm@alibaba-inc.com>
2017-02-09 15:01:29 +0800

    feature. add framework and base code.

---
+++ webpack/webpack.depoly.config.js
@@ -0,0 +1,105 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+
+var config = {
+  context: path.join(__dirname, '..', '/root'),
+  entry: {
+    index: './index/start',
+    vendors: ['react', 'react-dom']
+  },
+  output: {
+    path: path.join(__dirname, '..', '/root/dist'),
+    publicPath: '//assets.isekai.me/',
+    filename: '[name].bundle.[hash].js',
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')), // judge if dev environment.
+      __PRERELEASE__: JSON.stringify(JSON.parse(process.env.BUILD_PRERELEASE || 'false')) // judge if secret environment.
+    }),
+    new uglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.MinChunkSizePlugin({ minChunkSize: 20000 }),
+    new webpack.optimize.OccurenceOrderPlugin(false),
+    new webpack.optimize.AggressiveMergingPlugin({
+      minSizeReduce: 1.5,
+      moveToParents: true
+    }),
+    new CommonsChunkPlugin('vendors', 'vendors.[hash].js', Infinity),
+    new ExtractTextPlugin("[name].[hash].css"),
+    new webpack.optimize.DedupePlugin(),
+    new HtmlWebpackPlugin({
+      template: './templates/index.deploy.html',
+      filename: 'index.html',
+      chunks: ['index', 'vendors'],
+      inject: 'body',
+      favicon: './assets/images/favicon.ico',
+      hash: true
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel"
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer?{browsers:["last 2 version", "> 1%"]}')
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer?{browsers:["last 2 version", "> 1%"]}!sass')
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        loader: 'url?limit=10000!img?progressive=true'
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg)$/,
+        loader: 'url?limit=10000'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.reactx$/,
+        loader: 'reactx-loader'
+      },
+    ]
+  },
+  resolve: {
+    // 設定後只需要寫 require('file') 而不用寫成 require('file.jsx')
+    extensions: ['', '.js', '.json', '.jsx', '.reactx', 'react']
+  },
+  externals: {
+    react: "React",
+    'react-dom': "ReactDOM",
+    redux: "Redux",
+    'react-redux': "ReactRedux",
+    lodash: "_",
+    wilddog: "wilddog",
+    marked: "marked",
+  },
+  reactx: {
+    // loaders for each langs
+    loaders: {
+      js: 'babel',
+      coffee: 'babel!coffee',
+      sass: 'style-loader!css-loader!autoprefixer?{browsers:["last 2 version", "> 1%"]}!sass'
+    },
+    // whether use source map
+    sourceMap: true
+  }
+};
+
+module.exports = config;
---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,111 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var config = {
+  context: path.join(__dirname, '..', '/root'),
+  entry: {
+    // Add each page's entry here
+    index: './index/start',
+  },
+  output: {
+    path: path.join(__dirname, '..', '/root/build'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')), // judge if dev environment.
+      __PRERELEASE__: JSON.stringify(JSON.parse(process.env.BUILD_PRERELEASE || 'false')) // judge if secret environment.
+    }),
+    new ExtractTextPlugin("[name].css"),
+    new HtmlWebpackPlugin({
+      template: './templates/index.dev.html',
+      filename: 'index.html',
+      chunks: ['index'],
+      favicon: './assets/images/favicon.ico',
+      inject: 'body'
+    }),
+  ],
+  module: {
+    perLoaders: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        loader: 'eslint'
+      },
+      {
+        test: /.reactx$/,
+        loader: 'eslint',
+        exclude: /node_modules/
+      },
+    ],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel"
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer?{browsers:["last 2 version", "> 1%"]}')
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!autoprefixer?{browsers:["last 2 version", "> 1%"]}!sass'
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        loader: 'url?limit=10000!img?progressive=true'
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg)$/,
+        loader: 'url?limit=10000'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.reactx$/,
+        loader: 'reactx-loader'
+      },
+    ],
+    noParse: []
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.jsx', '.reactx', 'react'],
+    alias: {}
+  },
+  devtool: 'eval-source-map',
+  jshint: {
+    "esnext": true
+  },
+  devServer: {
+    host: '0.0.0.0',
+    historyApiFallback: {
+      index: 'index.html',
+      rewrites: [
+        { from: /\/index/, to: '/index.html' },
+      ]
+    },
+    // proxy: {
+    //   '/api/v1/*': {
+    //     target: 'http://123.59.79.196',
+    //     secure: false
+    //   }
+    // }
+  },
+  reactx: {
+    // loaders for each langs
+    loaders: {
+      js: 'babel',
+      coffee: 'babel!coffee',
+      sass: 'style-loader!css-loader!autoprefixer?{browsers:["last 2 version", "> 1%"]}!sass'
+    },
+    // whether use source map
+    sourceMap: true
+  }
+};
+
+module.exports = config;
