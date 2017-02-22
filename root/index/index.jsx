5b060967bf567bbf1e5eca5901c64f34f87c9452
锡炎 <xiyan.xxm@alibaba-inc.com>
2017-02-22 11:46:38 +0800

    feature. add background.

--- root/index/index.jsx
+++ root/index/index.jsx
@@ -7,20 +7,24 @@ export default class Index extends React.Component {
     super(props);
     this.state = {
       nameIndex: 0,
+      currentInfo: infos[names[0]],
+      background: infos[names[0]].background,
     };
   }
   handleIndexChange(change) {
     const nameIndex = this.state.nameIndex + change;
     if (nameIndex < 0 || nameIndex >= names.length) return;
+    const currentInfo = infos[names[nameIndex]];
     this.setState({
       nameIndex,
+      currentInfo,
+      background: currentInfo.background,
     });
   }
   render() {
     return (
-      <div id="container">
-        <Species info={infos[names[this.state.nameIndex]]} />
-        
+      <div id="container" style={{ backgroundColor: this.state.background }}>
+        <Species info={this.state.currentInfo} />
         <div className="control-pad">
           <button onClick={this.handleIndexChange.bind(this, -1)}>Previous</button>
           <button onClick={this.handleIndexChange.bind(this, 1)}>Next</button>
